ec57ecc4fee719de710b28334d8e39b8754b51ff
Jalvarez025 <90490928+Jalvarez025@users.noreply.github.com>
2021-11-29 13:31:36 -0600

    Offline works

---
+++ public/js/idb.js
@@ -0,0 +1,54 @@
+const indexedDB = window.indexedDB || window.mozIndexedDB || webkitIndexedDB || msIndexedDB || shimIndexedDB
+let db;
+const request = indexedDB.open("budget", 1)
+
+request.onupdgradeneeded = ({ target }) => {
+    let db = target.result;
+    db.createObjectStore("pending",{ autoIncrement: true})
+} 
+
+request.onsuccess = ({ target }) => {
+    db = target.result;
+    if(navigator.onLine){
+        checkDatabase();
+    }
+}
+
+request.onerror = (evt) => {
+    console.log(evt.target.errorCode)
+}
+
+function saveRecord(record){
+    const transaction = db.transaction(["pending"], "readwrite")
+    const store = transaction.objectStore("pending")
+    store.add(record)
+}
+
+function checkDatabase () {
+    const transaction = db.transaction(["pending"], "readwrite")
+    const store = transaction.objectStore("pending")
+    const getAll = store.getAll()
+
+    getAll.onsuccess = () => {
+        if (getAll.result.length > 0) {
+            fetch("/api/transaction/bulk", {
+                method: "POST",
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: "application/json, text/plain, */*",
+                    "Content-Type": "application/json"
+                }
+            })
+            .then((res) => res.json())
+                .then(() => {
+                    const transaction = db.transaction(["pending"], "readwrite")
+                    const store = transaction.objectStore("pending")
+                    store.clear()
+                })
+        }
+    }
+
+   
+}
+
+window.addEventListener('online', checkDatabase)
